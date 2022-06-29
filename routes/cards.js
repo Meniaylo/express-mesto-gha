c72ce5ced6ec97a386880833fc9a0b6ae75bdb1e
yzma <upthere@yandex.ru>
2022-06-29 15:09:24 +0300

    new structure and linter

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,14 @@ const {
   createCard,
   cardsController,
   deleteCard,
+  putCardLike,
+  deleteCardLike
 } = require("../controllers/cards");
 
 cardRouter.get("/", cardsController);
 cardRouter.delete("/:cardId", deleteCard);
 cardRouter.post("/", createCard);
+cardRouter.put("/:cardId/likes", putCardLike)
+cardRouter.delete("/:cardId/likes", deleteCardLike);
 
 module.exports = cardRouter;
