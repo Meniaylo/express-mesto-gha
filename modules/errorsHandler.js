b3c6a831f54d5a856beeb192acdde10ca61d595d
yzma <upthere@yandex.ru>
2022-08-16 22:06:32 +0300

    hotfix: fix errorsHandler func

--- modules/errorsHandler.js
+++ modules/errorsHandler.js
@@ -1,7 +1,8 @@
-const errorsHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errorsHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  res
+  return res
     .status(statusCode)
     .send({
       message: statusCode === 500
